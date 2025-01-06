40cc138f6f9c1084f2f60977de87325abddaf6f6
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2025-01-06 10:12:55 -0500

    .

---
+++ hunter.py
@@ -0,0 +1,3 @@
+import pygame
+
+from item import Item
--- item.py
+++ item.py
@@ -3,6 +3,11 @@ import pygame
 from shapes import Cylinder
 
 class Item(Cylinder):
+    """"""
+
     def __init__(self, x, y, start_z, end_z, radius, type):
         super().__init__(x, y, start_z, end_z, radius)
         self.type = type
+
+    def get_type(self):
+        return self.type
