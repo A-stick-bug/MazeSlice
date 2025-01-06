22e65e0ed64fee0e20ff7fcd3c02dceb4e353b81
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2025-01-06 09:46:48 -0500

    Update item.py

--- item.py
+++ item.py
@@ -1,1 +1,7 @@
-# test
+import pygame
+
+from shapes import Cylinder
+
+class Item(Cylinder):
+    def __init__(self, x, y, start_z, end_z, radius):
+        super().__init__(x, y, start_z, end_z, radius)
