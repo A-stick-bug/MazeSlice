1595c9625da8b02d9c38167f904edc07c2b80d7f
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2025-01-08 10:16:37 -0500

    Update hunter.py

--- hunter.py
+++ hunter.py
@@ -6,7 +6,6 @@ from random import random
 from main import Player
 from shapes import Circle
 
-
 class Hunter(Circle):
     """"""
 
@@ -14,19 +13,24 @@ class Hunter(Circle):
         super().__init__(x, y, z, radius)
         self.speed = speed
 
+    def z_distance_from_player(self, player: Player):
+        return abs(self.z, player.get_z())
+
     def handle_movement(self, player: Player) -> None:
         """"""
         # maybe just let hunter move freely through obstacles
 
-        player_location = player.get_location()[:2]
-        cur_location = (self.x, self.y)
-        distance_from_player = dist(player_location, cur_location)
-        movement_scalar = player.speed / distance_from_player
-        self.x += (player.get_x() - self.x) * movement_scalar
-        self.y += (player.get_y() - self.y) * movement_scalar
-        if self.z > player.get_z():
-            if random.random() > self.speed / 10:
-                self.z -= 1
-        elif self.z < player.get_z():
-            if random.random() > self.speed / 10:
-                self.z += 1
+        if self.z_distance_from_player(player) <= 20:
+            player_location = player.get_location()[:2]
+            cur_location = (self.x, self.y)
+            distance_from_player = dist(player_location, cur_location)
+            movement_scalar = player.speed / distance_from_player
+            self.x += (player.get_x() - self.x) * movement_scalar
+            self.y += (player.get_y() - self.y) * movement_scalar
+            if self.z > player.get_z():
+                if random.random() > self.speed / 10:
+                    self.z -= 1
+            elif self.z < player.get_z():
+                if random.random() > self.speed / 10:
+                    self.z += 1
+            # separate z movement from xy, and cheap non integral speed implementation
