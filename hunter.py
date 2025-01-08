9c08b7e8dfa83a07661bd99953683ea46e3e9ce8
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2025-01-08 00:38:49 -0500

    Update hunter.py

--- hunter.py
+++ hunter.py
@@ -1,27 +1,32 @@
-import pygame
+# hunter.py
 
 from math import dist
+from random import random
 
-from item import Item
 from main import Player
+from shapes import Circle
 
-class Hunter(Item):
+
+class Hunter(Circle):
     """"""
-    
-    def __init__(self, x, y, start_z, end_z, radius, speed = 0.0):
-        super().__init__(x, y, start_z, end_z, radius, 0)
+
+    def __init__(self, x, y, z, radius, speed=0.0):
+        super().__init__(x, y, z, radius)
         self.speed = speed
 
     def handle_movement(self, player: Player) -> None:
         """"""
         # maybe just let hunter move freely through obstacles
 
-        player_location = player.get_location()
-        mid_z = (self.start_z + self.end_z) / 2
-        cur_location = (self.x, self.y, mid_z) # may be replaced by self.get_location()
+        player_location = player.get_location()[:2]
+        cur_location = (self.x, self.y)
         distance_from_player = dist(player_location, cur_location)
         movement_scalar = player.speed / distance_from_player
         self.x += (player.get_x() - self.x) * movement_scalar
         self.y += (player.get_y() - self.y) * movement_scalar
-        self.start_z += (player.get_x() - mid_z) * movement_scalar
-        self.end_z += (player.get_x() - mid_z) * movement_scalar
+        if self.z > player.get_z():
+            if random.random() > self.speed / 10:
+                self.z -= 1
+        elif self.z < player.get_z():
+            if random.random() > self.speed / 10:
+                self.z += 1
